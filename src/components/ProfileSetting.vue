30211cf24c55ab47ffb6e46b0cab2f6c4b068f4b
pseuder <iop890520@gmail.com>
2023-02-27 10:29:00 +0800

    Add ProfileSetting component with AvatarUpload, EditableText, and userData variables.

---
+++ src/components/ProfileSetting.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from "vue";
+import AvatarUpload from "@/components/AvatarUpload.vue";
+import EditableText from "@/components/EditableText.vue";
+
+let userData = reactive({
+  name: "Leman",
+  account: "a123456",
+  password: "a12*****456",
+  email: "a123456@mail.com",
+  phone: "0912345678",
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <AvatarUpload />
+    <EditableText type="name" :data="userData.name" />
+    <EditableText type="account" :data="userData.account" />
+    <EditableText type="password" :data="userData.password" />
+    <EditableText type="email" :data="userData.email" />
+    <EditableText type="phone" :data="userData.phone" />
+  </div>
+</template>
