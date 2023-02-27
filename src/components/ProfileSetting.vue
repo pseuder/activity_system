e7c3980a223428fa4f69cbab10983dca8b038bff
pseuder <iop890520@gmail.com>
2023-02-27 22:23:46 +0800

    Update ProfileSetting.vue to include user service to fetch user data, and add functions for updating user data.

--- src/components/ProfileSetting.vue
+++ src/components/ProfileSetting.vue
@@ -1,24 +1,57 @@
 <script setup>
-import { reactive } from "vue";
+import { reactive, onBeforeMount } from "vue";
 import AvatarUpload from "@/components/AvatarUpload.vue";
 import EditableText from "@/components/EditableText.vue";
+import UserService from "@/services/user.service.js";
 
 let userData = reactive({
-  name: "Leman",
-  account: "a123456",
-  password: "a12*****456",
-  email: "a123456@mail.com",
-  phone: "0912345678",
+  _id: "",
+  avatar: "",
+  createDate: "",
+  email: "",
+  password: "",
+  username: "",
+  phone: "",
 });
+
+onBeforeMount(async () => {
+  await UserService.getUserInfo().then((res) => {
+    for (let key in userData) {
+      userData[key] = res.data[key];
+    }
+  });
+});
+
+function changeUserData(key, val) {
+  userData[key] = val;
+}
 </script>
 
 <template>
   <div class="flex flex-col items-center">
-    <AvatarUpload />
-    <EditableText type="name" :data="userData.name" />
-    <EditableText type="account" :data="userData.account" />
-    <EditableText type="password" :data="userData.password" />
-    <EditableText type="email" :data="userData.email" />
-    <EditableText type="phone" :data="userData.phone" />
+    <AvatarUpload
+      :user-img="userData.avatar"
+      @change-user-data="changeUserData"
+    />
+    <EditableText
+      type="username"
+      :data="userData.username"
+      @change-user-data="changeUserData"
+    />
+    <EditableText
+      type="email"
+      :data="userData.email"
+      @change-user-data="changeUserData"
+    />
+    <EditableText
+      type="password"
+      :data="userData.password"
+      @change-user-data="changeUserData"
+    />
+    <EditableText
+      type="phone"
+      :data="userData.phone"
+      @change-user-data="changeUserData"
+    />
   </div>
 </template>
