cca92670ade897fa861bb46cae452262ac9ef589
pseuder <iop890520@gmail.com>
2023-02-28 21:30:26 +0800

    Add MainTag component to provide filtering feature on click.

---
+++ src/components/MainTag.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  item: {
+    type: Object,
+    default() {
+      return {};
+    },
+  },
+  userSetting: {
+    type: Object,
+    default() {
+      return {};
+    },
+  },
+});
+
+defineEmits(["filterClick"]);
+</script>
+
+<template>
+  <div
+    class="tag mx-2 my-2 inline-flex w-20 lg:w-11/12 lg:max-w-[160px]"
+    :class="{
+      'text-white bg-primary': userSetting.selectedTag == item.name,
+    }"
+    @click="$emit('filterClick', item)"
+  >
+    {{ item.name }}
+  </div>
+</template>
