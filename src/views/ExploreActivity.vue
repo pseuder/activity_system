57f45ca55ade1d0dd4354747c23cf3079ea2adbb
pseuder <iop890520@gmail.com>
2023-02-28 21:30:59 +0800

    refactor

--- src/views/ExploreActivity.vue
+++ src/views/ExploreActivity.vue
@@ -1,11 +1,17 @@
 <script setup>
-import { reactive, ref, onBeforeMount } from "vue";
+import { reactive, ref, onBeforeMount, onMounted, watch } from "vue";
 import "tw-elements";
 
 import AlertMessage from "@/components/AlertMessage.vue";
 import ActivityCard from "@/components/ActivityCard.vue";
 import DetailDialog from "@/components/DetailDialog.vue";
 import EditDialog from "@/components/EditDialog.vue";
+import MainNav from "@/components/MainNav.vue";
+import MainSort from "@/components/MainSort.vue";
+import MainDisplay from "@/components/MainDisplay.vue";
+import MainTag from "@/components/MainTag.vue";
+import UserService from "@/services/user.service.js";
+
 import {
   sortStatus,
   sortOptions,
@@ -29,15 +35,25 @@ import {
 
 let groupData = [];
 let activityData = reactive([]);
-let activityData_display = reactive([]);
+let activityData_display = ref([]);
 let detialDialogData = ref({});
+let userData = ref({});
 let loading = ref(true);
 
 userSetting.displayMode = "list";
-userSetting.selectedTag = "全部";
+userSetting.selectedTag = "";
+
+// 根據標籤過濾
+watch(
+  () => userSetting.selectedTag,
+  () => {
+    activityData_display.value = activityData.filter((activity) =>
+      activity.object.includes(userSetting.selectedTag)
+    );
+  }
+);
 
 /* functions */
-
 function sortClick(option) {
   sorting({
     option,
@@ -46,10 +62,10 @@ function sortClick(option) {
   });
 }
 function filterClick(option) {
-  activityData_display = exploreActivityFiltering({
-    option,
-    activityData,
-  });
+  userSetting.selectedTag = option.name;
+}
+function displayClick(type) {
+  userSetting.displayMode = type;
 }
 function detailClick(activity) {
   detailing({ activity, detialDialogData });
@@ -73,24 +89,28 @@ function fetchData() {
     // 獲取標記後活動資料
     return fetchAndMarkActivityData().then((res) => {
       activityData = res;
-      return;
     });
   });
 }
 
 function removeActivityDisplay(activity) {
-  activityData_display.splice(activityData_display.indexOf(activity), 1);
-  activityData.splice(activityData_display.indexOf(activity), 1);
-  console.log(activityData);
-  console.log(activityData_display);
+  activityData_display.value.splice(
+    activityData_display.value.indexOf(activity),
+    1
+  );
+  activityData.splice(activityData.indexOf(activity), 1);
 }
 
 // hook
 onBeforeMount(() => {
+  UserService.getUserInfo().then((res) => {
+    userData.value = res.data;
+  });
+
   fetchData()
     .then(() => {
       // 將原始資料過濾成"已報名"的資料
-      filterClick({ name: "全部", attribute: "all" });
+      userSetting.selectedTag = "所有人";
       loading.value = false;
     })
     .catch((err) => {
@@ -121,65 +141,7 @@ onBeforeMount(() => {
 
     <div v-show="!loading">
       <header class="flex justify-between text-3xl font-medium lg:pt-6">
-        <div class="flex items-center">
-          <img src="@/assets/image/vite.svg" alt="vue logo" class="w-10" />
-        </div>
-        <nav
-          class="flex flex-grow items-center justify-center lg:ml-[10vw] lg:justify-start"
-        >
-          <div class="dropdown relative">
-            <button
-              id="dropdownMenuButton"
-              class="dropdown-toggle flex items-center text-primary lg:hidden"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              探索活動
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="caret-down"
-                class="ml-2 w-4"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
-                ></path>
-              </svg>
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <li>
-                <a class="dropdown-item text-primary" href="/explore"
-                  >探索活動</a
-                >
-              </li>
-              <li>
-                <a class="dropdown-item" href="/create">建立活動</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="/myactivity">我的活動</a>
-              </li>
-            </ul>
-          </div>
-
-          <div class="hidden lg:flex">
-            <div class="flex flex-shrink-0 gap-20 pl-[10vw] font-bold">
-              <a class="" href="/explore">探索活動</a>
-              <a class="text-black" href="/create">建立活動</a>
-              <a class="text-black" href="/myactivity">我的活動</a>
-            </div>
-          </div>
-        </nav>
-        <div class="flex items-center">
-          <a href="/setting">
-            <img src="@/assets/image/vue.svg" alt="vue logo" class="w-10" />
-          </a>
-        </div>
+        <MainNav :user-data="userData" />
       </header>
       <main class="py-4">
         <!-- sort bar -->
@@ -187,67 +149,18 @@ onBeforeMount(() => {
           <!-- 條件排序 -->
           <div class="flex flex-grow flex-wrap text-xl">
             <template v-for="(item, index) in sortOptions" :key="index">
-              <div
-                class="mx-6 my-2 inline-flex items-center justify-center gap-2"
-                @click="sortClick(item)"
-              >
-                <span
-                  class="cursor-pointer select-none font-semibold"
-                  :class="{
-                    '  text-blue-500': item.status != sortStatus.None,
-                  }"
-                  >{{ item.name }}</span
-                >
-                <div class="flex cursor-pointer flex-col gap-1">
-                  <svg
-                    width="12"
-                    height="9"
-                    viewBox="0 0 12 9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    :class="{
-                      ' fill-blue-500': item.status === sortStatus.UP,
-                    }"
-                  >
-                    <path
-                      d="M5.22149 0.410216L0.258056 7.00414C-0.327704 7.78251 0.148355 9 1.03734 9H10.9642C11.1632 9.0002 11.3579 8.93373 11.5252 8.80855C11.6925 8.68336 11.8252 8.50478 11.9075 8.29418C11.9897 8.08358 12.018 7.84988 11.9889 7.62109C11.9598 7.39229 11.8746 7.17809 11.7435 7.00414L6.78006 0.411419C6.68292 0.2822 6.56313 0.178633 6.42872 0.107673C6.29431 0.0367125 6.14839 0 6.00077 0C5.85315 0 5.70724 0.0367125 5.57283 0.107673C5.43842 0.178633 5.31862 0.2822 5.22149 0.411419V0.410216Z"
-                    />
-                  </svg>
-                  <svg
-                    width="12"
-                    height="9"
-                    viewBox="0 0 12 9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    :class="{
-                      ' fill-blue-500': item.status === sortStatus.DOWN,
-                    }"
-                  >
-                    <path
-                      d="M5.22149 8.58978L0.258056 1.99466C-0.327703 1.21869 0.148355 4.42369e-07 1.03734 4.42369e-07H10.9642C11.1632 -0.000197853 11.3579 0.0662735 11.5252 0.191455C11.6925 0.316636 11.8252 0.49522 11.9075 0.705822C11.9897 0.916423 12.018 1.15011 11.9889 1.37891C11.9598 1.60771 11.8746 1.82191 11.7435 1.99586L6.78006 8.58858C6.68292 8.7178 6.56313 8.82137 6.42872 8.89233C6.29431 8.96329 6.14839 9 6.00077 9C5.85315 9 5.70724 8.96329 5.57283 8.89233C5.43842 8.82137 5.31862 8.7178 5.22149 8.58858V8.58978Z"
-                    />
-                  </svg>
-                </div>
-              </div>
+              <MainSort
+                :item="item"
+                :sort-status="sortStatus"
+                @sort-click="sortClick"
+              />
             </template>
           </div>
           <!-- 排序模式 -->
           <div class="hidden justify-center gap-4 self-center lg:flex">
-            <img
-              src="@/assets/image/exploreList.svg"
-              alt="list"
-              class="h-8 w-8 cursor-pointer rounded-lg border-solid border-black p-1"
-              :class="{
-                'border-2': userSetting.displayMode == 'list',
-              }"
-              @click="userSetting.displayMode = 'list'"
-            />
-            <img
-              src="@/assets/image/exploreBlock.svg"
-              alt="block"
-              class="h-8 w-8 cursor-pointer rounded-lg border-solid border-black p-1"
-              :class="{
-                'border-2 ': userSetting.displayMode == 'block',
-              }"
-              @click="userSetting.displayMode = 'block'"
+            <MainDisplay
+              :user-setting="userSetting"
+              @display-click="displayClick"
             />
           </div>
         </div>
@@ -255,16 +168,12 @@ onBeforeMount(() => {
         <div class="lg:flex">
           <!-- tag過濾 -->
           <div class="flex flex-wrap font-semibold lg:w-1/5 lg:flex-col">
-            <template v-for="(item, index) in groupData" :key="index">
-              <div
-                class="tag mx-2 my-2 inline-flex w-20 lg:w-11/12 lg:max-w-[160px]"
-                :class="{
-                  'text-white bg-primary': userSetting.selectedTag == item.name,
-                }"
-                @click="filterClick(item)"
-              >
-                {{ item.name }}
-              </div>
+            <template v-for="(item, index) in groupData" :key="item._id">
+              <MainTag
+                :item="item"
+                :user-setting="userSetting"
+                @filter-click="filterClick"
+              />
             </template>
           </div>
           <!-- 活動顯示 -->
