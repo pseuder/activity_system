e84671d47664b7347d5a110ec24be911d915c25a
pseuder <iop890520@gmail.com>
2023-02-28 21:29:57 +0800

    Add MainDisplay component with user setting props and displayClick emit event.

---
+++ src/components/MainDisplay.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  userSetting: {
+    type: Object,
+    default() {
+      return {};
+    },
+  },
+});
+
+defineEmits(["displayClick"]);
+</script>
+
+<template>
+  <img
+    src="@/assets/image/exploreList.svg"
+    alt="list"
+    class="h-8 w-8 cursor-pointer rounded-lg border-solid border-black p-1"
+    :class="{
+      'border-2': userSetting.displayMode == 'list',
+    }"
+    @click="$emit('displayClick', 'list')"
+  />
+  <img
+    src="@/assets/image/exploreBlock.svg"
+    alt="block"
+    class="h-8 w-8 cursor-pointer rounded-lg border-solid border-black p-1"
+    :class="{
+      'border-2 ': userSetting.displayMode == 'block',
+    }"
+    @click="$emit('displayClick', 'block')"
+  />
+</template>
